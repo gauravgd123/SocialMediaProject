a6e69d43225145ce844457f661dac73227677b70
Gaurav Dangwal <gdangwal268@gmail.com>
2024-05-09 12:02:34 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SideBar from "./components/SideBar";
-import CreatePost from './components/CreatePost';
-import PostList from './components/PostList';
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+import PostListProvider from "./store/post-list-store";
 
-function App(){
-  const [selectedTab,setSelectedTab]=useState('Home');
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-
-    <div className="app-container">
-    <SideBar selectedTab={selectedTab} setSelectedTab = {setSelectedTab} />
-    <div className="content">
-     <Header />
-     {selectedTab === 'Home' ?( <PostList /> ): (<CreatePost /> )} 
-     <Footer />
-     </div>
-     
-     </div>
-
-  )
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListProvider>
+  );
 }
 
 export default App;
